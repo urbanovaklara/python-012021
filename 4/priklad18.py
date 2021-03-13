ae28b9ed51af7372d066fdc38527539578bc29af
urbanovaklara <urbanovaklara@yahoo.com>
2021-03-13 22:59:00 +0100

    verze1

---
+++ 4/priklad18.py
@@ -0,0 +1,23 @@
+from datetime import datetime, date
+
+class Kontakt:
+    def __init__(self, jmeno, email):
+        self.jmeno = jmeno
+        self.email = email
+
+class Uchazec(Kontakt):
+    def __init__(self, jmeno, email, datum_pohovoru, zapis_z_pohovoru=""):
+        super().__init__(jmeno, email)
+        self.datum_pohovoru = datetime.date(datum_pohovoru)
+
+    def uloz_zapis(self):
+        today = datetime.today()
+        if today < date(datum_pohovoru):
+            return f"Zápis z pohovoru je ukládán k uchazeči, s nímž pohovor ještě neproběhl."
+        else:
+            zapis_z_pohovoru = input(str(""))
+            return f"Zápis byl uložen"
+
+uchazec1 = Uchazec ("Klára Urbanová", "klara@seznam.cz", "22.3.2021")
+print(uchazec1.uloz_zapis())
+
